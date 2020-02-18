a8c13dab93a3e09a0a996f154fcc7e2d817f9fe3
Robert <robertneagu194@gmail.com>
2020-02-18 20:44:23 +0000

    Add the parsing of the XML on server startup

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,18 @@
+const fs = require('fs');
 const express = require('express');
+const xml2json = require('xml2json');
 
 const router = express.Router();
 
+const api = {};
+/* Read the XML file and parse it into a Javascript Object */
+fs.readFile('./data/flightdata_A.xml', (err, data) => {
+  api.flights = xml2json.toJson(data, { object: true });
+});
+
 /* GET: /api/test endpoint */
 router.get('/test', async (req, res) => {
-  res.json({ data: 'test successful' });
+  res.json(api.flights);
 });
 
 module.exports = router;
