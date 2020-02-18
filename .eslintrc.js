aeb829554cd91e928ed56d665d546af7154a3e91
Robert <robertneagu194@gmail.com>
2020-02-18 21:51:09 +0000

    Add new rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,7 @@ module.exports = {
     "react/jsx-filename-extension": [ 1, { "extensions": [".js", ".jsx"] } ], // allows JSX content to be saved as .js files
     'max-len': [ "error", { "code": 180 } ], // sets the max character allowed in a line to 180 characters
     'linebreak-style': 0, // removes the restriction on the linebreak
+    'camelcase': 0, // removes the camelcase restriction for the variable names
+    'radix': 0, // removes the radix restriction for the parseInt function
   },
 };
