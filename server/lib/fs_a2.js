18104d5cd485493692966323d2324bdababedcdf
Robert <robertneagu194@gmail.com>
2020-02-18 21:52:22 +0000

    Add function to count all the morning flights

--- server/lib/fs_a2.js
+++ server/lib/fs_a2.js
@@ -3,6 +3,21 @@ const xml2json = require('xml2json');
 
 const fs_a2 = {
   flights: {},
+  /**
+   * Counts all the morning flights (assuming morning is between 6:00 and 12:00 AM)
+   */
+  countAllMorningFlights() {
+    const filtered = this.flights.filter((flight) => {
+      const [inDepartureHour] = flight.indeparttime.split(':');
+      const [outDepartureHour] = flight.outdeparttime.split(':');
+
+      const boolInDepartureMorning = (Number.parseInt(inDepartureHour) >= 6 && Number.parseInt(inDepartureHour) < 12);
+      const boolOutDepartureMorning = (Number.parseInt(outDepartureHour) >= 6 && Number.parseInt(outDepartureHour) < 12);
+
+      return boolInDepartureMorning || boolOutDepartureMorning;
+    });
+    return filtered.length;
+  },
 };
 
 /**
