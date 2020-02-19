36a951106813ced6d94c2bc8c29cfcf90b2016a1
Robert <robertneagu194@gmail.com>
2020-02-19 00:06:30 +0000

    Remove test function

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 /* GET: /api/test endpoint */
 router.get('/test', async (req, res) => {
-  console.log(fs_a2.countAllMorningFlights());
   res.json(fs_a2.flights);
 });
 
